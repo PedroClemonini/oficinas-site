93be72821e98b294d167da96af8ad268b8c05e8b
Pedro Clemonini <pclemonini-00@hotmail.com>
2026-03-05 21:10:31 -0300

    docker add

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4lkBEv+lQAsPMwMRX6GPu7+Y4nt6BENPIxIZXGOcZ9g=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QEZXtV1wlgRymG7SzrIjOGz+C+ts+3++XUkOmKo9t/I=\"\n}"
